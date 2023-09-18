9935a1889088ed0cb5dbf85610b5635bba07ecb9
Daniel Toman <116273746+Dan21104@users.noreply.github.com>
2023-09-18 22:25:54 +0200

    Update 01_Hello_World.py

--- 01_Basics/01_Hello_World.py
+++ 01_Basics/01_Hello_World.py
@@ -1,17 +1,25 @@
-print("Hello World! \n ~ My name is Wi.")
-name = input("What is your name?\n >")
-print("Nice to meet you " + name + ", how old are you?")
+print("Hello World!\n~ My name is Wi.")
+name = input("What is your name?\n> ")
+print("Nice to meet you, " + name + "! How old are you?")
+
 while True:
     try:
-        age = int(input(" >"))
+        age = int(input("> "))
         if age <= 13:
-            print("You have to be older than 13 y.o.")
+            print("You have to be older than 13 years old.")
             continue
         elif age > 116:
-            print("That is not real.")
+            print("That doesn't seem likely. Please enter a valid age.")
             continue
         else:
             break
     except ValueError:
-        print("Try again, insert eligible number.")
+        print("Invalid input. Please enter a valid age.")
         continue
+
+print("Great! You are " + str(age) + " years old.")
+hobbies = input("What are your hobbies?\n> ")
+print("Interesting! You enjoy " + hobbies + ".")
+location = input("Where are you from?\n> ")
+print("I've heard " + location + " is a beautiful place.")
+print("Unfortunately, I have to go now, " + name + ".")
